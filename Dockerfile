b582ffe06ad8420f5f1e7a8de2c9ea97c1868c06
Philippe Hässig <phil@nine.ch>
2018-01-19 13:17:56 +0100

    Add mysql connection

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM php:7.2-apache
 
+RUN docker-php-ext-install mysqli
+
 # Change Apache's port to 8000 so it does not need root
 RUN echo "Listen 8000" > /etc/apache2/ports.conf
 RUN chmod a+w /var/run/apache2
--- index.php
+++ index.php
@@ -10,5 +10,20 @@
 
   <?php echo "Hello World, have a great " . date("l") . "!"; ?>
 
+  <?php
+    $mysqli = new mysqli(getenv("MYSQL_HOST"), getenv("MYSQL_USER"), getenv("MYSQL_PASSWORD"), getenv("MYSQL_DATABASE"));
+    if ($mysqli->connect_errno) {
+      die("Database connection failed: " . $mysqli->connect_error);
+    }
+
+    echo "<p>Connection to mysql established.</p>\n";
+
+    $statement = $mysqli->prepare("SELECT VERSION() as version;");
+    $statement->execute();
+    $result = $statement->get_result();
+
+    $row = $result->fetch_assoc();
+    echo "<p>MySQL version: " . $row["version"] . "</p>\n";
+  ?>
 </body>
 </html>
